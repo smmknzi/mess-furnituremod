2e2b347bcd0d862f3ede70c851ea21d7f7de0aa5
mess <smithmackenzie585@gmail.com>
2026-06-18 11:11:26 -0400

    Update to 26.2

--- src/main/java/net/mess/furnituremod/block/ModBlocks.java
+++ src/main/java/net/mess/furnituremod/block/ModBlocks.java
@@ -8,13 +8,11 @@ import net.mess.furnituremod.block.custom.TableBlock;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.*;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import java.util.function.Function;
